eb9e6b02bc4beb12e7a41f0901177e9e392ed9c0
Brice Powell <bpowell@healthrecoverysolutions.com>
2026-04-28 13:22:37 -0400

    Remove check for requires non null

--- src/android/com/hrs/firebase/messaging/FCMPluginActivity.java
+++ src/android/com/hrs/firebase/messaging/FCMPluginActivity.java
@@ -71,7 +71,7 @@ public class FCMPluginActivity extends Activity {
 
         Timber.d("==> USER TAPPED NOTIFICATION");
         Map<String, Object> data;
-        Object object = Objects.requireNonNull(intentExtras.get("data"));
+        Object object = intentExtras.get("data");
         if (object instanceof Bundle) {
             data = Utils.bundleToHashMap((Bundle) object);
         } else {
